e596ce94ae7449377259c2af151a5fc86e79212f
damodar <damodargouda99@gmail.com>
2026-07-12 20:34:58 +0530

    Add product search functionality

--- inventory/src/main/java/com/tyson/inventory/repository/ProductRepository.java
+++ inventory/src/main/java/com/tyson/inventory/repository/ProductRepository.java
@@ -3,6 +3,10 @@ package com.tyson.inventory.repository;
 import com.tyson.inventory.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findByProductNameContainingIgnoreCase(String keyword);
 
 }
