ac113427f572c227a69e734a30119a73534d7b00
damodar <damodargouda99@gmail.com>
2026-07-12 20:21:23 +0530

    Added Spring Boot project structure

---
+++ inventory/src/main/java/com/tyson/inventory/controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.tyson.inventory.controller;
+
+import com.tyson.inventory.entity.Product;
+import com.tyson.inventory.repository.ProductRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class ProductController {
+
+    private final ProductRepository productRepository;
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model) {
+        model.addAttribute("products", productRepository.findAll());
+        return "products";
+    }
+
+    @GetMapping("/add-product")
+    public String addProductPage(Model model) {
+        model.addAttribute("product", new Product());
+        return "add-product";
+    }
+
+    @PostMapping("/save-product")
+    public String saveProduct(@ModelAttribute Product product) {
+        productRepository.save(product);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/delete-product/{id}")
+    public String deleteProduct(@PathVariable Long id) {
+        productRepository.deleteById(id);
+        return "redirect:/products";
+    }
+
+    @GetMapping("/edit-product/{id}")
+    public String editProduct(@PathVariable Long id, Model model) {
+        Product product = productRepository.findById(id).orElse(null);
+        model.addAttribute("product", product);
+        return "add-product";
+    }
+}
---
+++ inventory/src/main/java/com/tyson/inventory/entity/Product.java
@@ -0,0 +1,70 @@
+package com.tyson.inventory.entity;
+
+import jakarta.persistence.*;
+
+    @Entity
+    @Table(name="products")
+    public class Product {
+
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        private String productName;
+
+        private String category;
+
+        private double price;
+
+        private int quantity;
+
+
+        public Long getId() {
+            return id;
+        }
+
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+
+
+        public String getProductName() {
+            return productName;
+        }
+
+
+        public void setProductName(String productName) {
+            this.productName = productName;
+        }
+
+
+        public String getCategory() {
+            return category;
+        }
+
+
+        public void setCategory(String category) {
+            this.category = category;
+        }
+
+
+        public double getPrice() {
+            return price;
+        }
+
+
+        public void setPrice(double price) {
+            this.price = price;
+        }
+
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
